04953032f59987cfdca4315166d6dc1085d5a774
catherina-alskaff <catherine.alskaf@gmail.com>
2023-10-11 16:11:48 +0200

    list transactions integration

--- app-frontend/app/components/transaction-history.js
+++ app-frontend/app/components/transaction-history.js
@@ -1,101 +1,38 @@
-// app/components/transaction-history.js
+
+//  app/components/transaction-history.js
 import { inject as service } from '@ember/service';
-import  Component  from '@ember/component';
-
-
-export default Component.extend({
-  apiService: service(),
-  
+import Component from '@ember/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+export default class TransactionHistoryComponent extends Component {
+  @service apiService;
+  // Hardcoded array
+  @tracked
+  transactions = [
+    { name: 'Item1', value: 100 },
+    { name: 'Item2', value: 200 },
+    { name: 'Item3', value: 300 },
+  ];
   async didInsertElement() {
-    this._super(...arguments);
-
+    super.didInsertElement(...arguments);
     this.transactions = await this.apiService.get('http://127.0.0.1:3000/api/v1/transactions');
     console.log(this.transactions);
-  },
-
-  actions: {
-    async handleSubmit(event) {
-      event.preventDefault();
-
-      // Get the account ID and amount from the form fields
-      const accountId = this.element.querySelector('#account-id').value;
-      const amount = this.element.querySelector('#amount').value;
-
-      // Create a body object with the account ID and amount
-      const body = {
-        account_id: accountId,
-        amount: amount
-      };
-
-      // Make a POST request to the API to create a new transaction
-      await this.apiService.post('http://127.0.0.1:3000/api/v1/transactions', body);
-
-      // Clear the form
-      this.element.querySelector('#account-id').value = '';
-      this.element.querySelector('#amount').value = '';
-    }
+    // Pass the transactions variable to the component as an argument
+    // this.args.transactions = this.transactions;
   }
-  
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // app/components/form.js
-// import Component from '@ember/component';
-// import { service } from '@ember/service';
-
-// export default Component.extend({
-//   apiService: service(),
-
-//   transaction: {
-//     accountId: null,
-//     amount: null,
-//   },
-
-//   actions: {
-//     async submit() {
-//       const transaction = {
-//         accountId: this.transaction.accountId,
-//         amount: this.transaction.amount,
-//       };
-
-//       await this.apiService.post('http://localhost:3000/api/v1/transactions', transaction);
-
-//       // Clear the form
-//       this.transaction = {
-//         accountId: null,
-//         amount: null,
-//       };
-//     }
-//   }
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
+  Actions
+  @action
+  async handleSubmit(event) {
+    event.preventDefault();
+    const accountId = this.element.querySelector('#account-id').value;
+    const amount = this.element.querySelector('#amount').value;
+    const body = {
+      account_id: accountId,
+      amount: amount,
+    };
+    await this.apiService.post('http://127.0.0.1:3000/api/v1/transactions', body);
+    this.transactions = await this.apiService.get('http://127.0.0.1:3000/api/v1/transactions');
+    this.element.querySelector('#account-id').value = '';
+    this.element.querySelector('#amount').value = '';
+  }
+}
